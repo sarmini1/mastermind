11b0d14a8347f0ecca2aa969fbc498e0671d2530
Spencer Armini <spencerarmini@gmail.com>
2023-11-24 14:56:33 -0800

    added most basic game logic and routes
    added various instance methods to handle guesses and scoring words, plus most necessary routes for the game.

--- app.py
+++ app.py
@@ -1,11 +1,88 @@
 import os
 
-from flask import Flask, request, render_template, jsonify, session
+from flask import Flask, request, render_template, session, redirect, flash
+from uuid import uuid4
+
+from mastermind import Mastermind
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 
-# games = {}
+curr_game = {}
+# TODO: implement session so that multiple folks can play
 
-# @app.get("/")
-# def homepage():
+
+@app.get("/")
+def homepage():
+    """
+    On GET, render a template that includes a button to start a new game.
+    """
+
+    return render_template("home.html")
+
+
+@app.post("/new-game")
+def start_new_game():
+    """
+    On POST, start a new game of Mastermind and redirect to gameplay template.
+    """
+
+    game = Mastermind()
+    game_id = str(uuid4())
+    curr_game[game_id] = game
+    flash("New game started!")
+
+    return redirect(f"/play/{game_id}")
+
+
+@app.get("/play/<game_id>")
+def play_game(game_id):
+    """
+    On GET, renders a template for the user to input their guess for the specified
+    game.
+    """
+
+    game = curr_game[game_id]
+    remaining_guesses = game.remaining_guesses
+    guess_history = game.guessed_nums_history
+    feedback = game.feedback
+
+    return render_template(
+        "gameplay.html",
+        game_id=game_id,
+        remaining_guesses=remaining_guesses,
+        guess_history=guess_history,
+        feedback=feedback
+    )
+
+
+@app.post("/submit-guess/<game_id>")
+def submit_guess(game_id):
+    """
+    On POST, extract form data and score incoming guess for the specified
+    game.
+    """
+
+    try:
+        first_num = int(request.form["first-num"])
+        second_num = int(request.form["second-num"])
+        third_num = int(request.form["third-num"])
+        fourth_num = int(request.form["fourth-num"])
+
+    except ValueError:
+        flash("Your guess is invalid; you can only input integers here.")
+        return redirect(f"/play/{game_id}")
+
+    breakpoint()
+    game = curr_game[game_id]
+    guessed_nums = [first_num, second_num, third_num, fourth_num]
+
+    game.handle_guess(guessed_nums)
+
+    if game.has_won:
+        return redirect("win.html")
+
+    if game.game_over:
+        return redirect("lose.html")
+
+    return redirect(f"/play/{game_id}")
--- mastermind.py
+++ mastermind.py
@@ -8,7 +8,13 @@ class Mastermind():
         """What needs to happen when a game instance is made."""
 
         self.answer = self.fetch_random_nums()
-        self.score = 0
+        # self.score = 0
+        self.has_won = False
+        self.game_over = False
+        self.guessed_nums_history = []
+        self.feedback = []
+        self.remaining_guesses = 10
+
         # self.difficulty = difficulty # TODO: add this once basic game is made
 
     @classmethod
@@ -26,10 +32,119 @@ class Mastermind():
 
         """
 
+        response = requests.get(
+            "https://www.random.org/integers/",
+            params={
+                "num": count,
+                "min": 1,
+                "max": 8,
+                "col": 1,
+                "base": 10,
+                "format": "plain",
+                "rnd": "new",
+            }
+        )
+
+        parsed_response = response.text.splitlines()
+        parsed_nums = [int(str) for str in parsed_response]
+
+        return parsed_nums
+
     def handle_guess(self, guessed_nums):
         """
-        Takes in a set of guessed numbers in a list, compares them to
-        the hidden numbers, and updates sco
+        Takes in a set of guessed numbers, scores them, and updates the game
+        instance accordingly:
+
+        Always:
+
+            - Stores their guess in the guessed_nums_history property
+            - Stores their feedback in the property by the same name
+            - Decrements remaining_guesses by 1
+
+        If this was their last remaining guess:
+            - Sets game_over to True
+
+        If their guess is 100% correct:
+            - Updates the has_won property to True
+            - Sets game_over to True
 
         """
 
+        score = self.score_guess(guessed_nums)
+        self.guessed_nums_history.append(guessed_nums)
+
+        feedback = self._generate_feedback(score)
+        self.feedback.append(feedback)
+
+        self.remaining_guesses -= 1
+
+        if self.remaining_guesses == 0:
+            self.game_over = True
+
+        if score["won"]:
+            self.game_over = True
+            self.has_won = True
+
+    def _generate_feedback(self, score):
+        """Returns a user_friendly string of information about their guess."""
+
+        correct_nums = score["correct_nums"]
+        correct_locations = score["correct_locations"]
+
+        if correct_nums == 0 and correct_locations == 0:
+            return "All incorrect."
+
+        if correct_nums == 4 and correct_locations == 4:
+            return "You win!"
+
+        return f"{correct_nums} correct number and {correct_locations} correct locations"
+
+    def score_guess(self, guessed_nums):
+        """
+        Takes in a set of guessed numbers in a list, compares them to
+        the hidden numbers, and returns the correct number and correct location
+        counts in a dictionary.
+
+        For example, if the hidden numbers were [1,5,7,8]:
+
+        Input:
+        guessed_nums = [1,2,3,4]
+
+        Output:
+        {
+            correct_nums: 1,
+            correct_locations: 1,
+        }
+        """
+
+        # iterate through guessed_nums [1,2,3,4]
+        # at each point, check to see if the value at the current index is
+        # equal to the value at the index in the answer list
+
+        # if so, increment both correct_nums and correct_locations counters by 1
+
+        # else, check if the current guessed number exists in the list at all
+        # if so, increment correct_nums counter
+
+        # TODO: consider making this an instance property
+        # Using a set will allow us to have a more efficient lookup if we need
+        # to search through the entire list of numbers.
+        answer_as_set = set(self.answer)
+
+        correct_nums = 0
+        correct_locations = 0
+        won = False
+        curr_index = 0
+
+        for num in guessed_nums:
+            if self.answer[curr_index] == num:
+                correct_nums += 1
+                correct_locations += 1
+            elif num in answer_as_set:
+                correct_nums += 1
+
+        return {
+            "won": won,
+            "correct_nums": correct_nums,
+            "correct_locations": correct_locations
+        }

1a6991029d6a9fbfbf866042b3104fccbd3e1e16
Spencer Armini <spencerarmini@gmail.com>
2023-11-25 00:31:41 -0800

    added difficulty and dupe handling to Mastermind class

--- mastermind.py
+++ mastermind.py
@@ -1,21 +1,22 @@
+from collections import Counter
+
 import requests
 
 
 class Mastermind():
     "The Mastermind game."
 
-    def __init__(self):
+    def __init__(self, count):
         """What needs to happen when a game instance is made."""
 
-        self.answer = self.fetch_random_nums()
+        self.answer = self.fetch_random_nums(count)
         # self.score = 0
         self.has_won = False
         self.game_over = False
         self.guessed_nums_history = []
         self.feedback = []
         self.remaining_guesses = 10
-
-        # self.difficulty = difficulty # TODO: add this once basic game is made
+        self.count = count
 
     @classmethod
     def create_new_game(cls):
@@ -36,8 +37,8 @@ class Mastermind():
             "https://www.random.org/integers/",
             params={
                 "num": count,
-                "min": 1,
-                "max": 8,
+                "min": 0,
+                "max": 7,
                 "col": 1,
                 "base": 10,
                 "format": "plain",
@@ -94,7 +95,7 @@ class Mastermind():
         if correct_nums == 0 and correct_locations == 0:
             return "All incorrect."
 
-        if correct_nums == 4 and correct_locations == 4:
+        if correct_nums == self.count and correct_locations == self.count:
             return "You win!"
 
         return f"{correct_nums} correct number and {correct_locations} correct locations"
@@ -112,39 +113,84 @@ class Mastermind():
 
         Output:
         {
-            correct_nums: 1,
-            correct_locations: 1,
+            "won": False,
+            "correct_nums": 1,
+            "correct_locations": 1,
         }
         """
-
-        # iterate through guessed_nums [1,2,3,4]
-        # at each point, check to see if the value at the current index is
-        # equal to the value at the index in the answer list
-
-        # if so, increment both correct_nums and correct_locations counters by 1
-
-        # else, check if the current guessed number exists in the list at all
-        # if so, increment correct_nums counter
-
         # TODO: consider making this an instance property
+
         # Using a set will allow us to have a more efficient lookup if we need
         # to search through the entire list of numbers.
-        answer_as_set = set(self.answer)
-
-        correct_nums = 0
-        correct_locations = 0
         won = False
         curr_index = 0
+        correct_nums = 0
+        correct_locations = 0
+
+        answer_as_set = set(self.answer)
+        frequencies_in_answer = Counter(self.answer)
+        correct_counters = {
+            key: {
+                "correct_nums": 0,
+                "correct_locations": 0
+            } for key in answer_as_set
+        }
+
+        # scrap while figuring out strategy for dupes
+        # {
+        # 1: {correct_nums: 1, correct_locations: 1}
+        # 4: {correct_nums: 2, correct_locations: 1}
+        # }
+        #
+
+        # {
+        # 0: {correct_nums: 0, correct_locations: 0}
+        # 1: {correct_nums: 1, correct_locations: 0}
+        # 3: {correct_nums: 0, correct_locations: 0}
+        # 5: {correct_nums: 0, correct_locations: 0}
+        # }
+        # guess  [4, 4, 1, 2] 3 correct numbers and 2 correct locations
+        # answer [1, 4, 1, 4]
+
+        # {
+        # 1: {correct_nums: 0, correct_locations: 0}
+        # 4: {correct_nums: 2, correct_locations: 1}
+        # }
+        #
+
+        # guess  [4, 4, 4, 4] 2 correct numbers and 2 correct locations
+        # answer [1, 4, 1, 4]
+
+        # guess  “2 2 1 1”, game responds “1 correct number and 0 correct location”
+        # answer “0 1 3 5”
 
         for num in guessed_nums:
+
             if self.answer[curr_index] == num:
-                correct_nums += 1
-                correct_locations += 1
+                curr_correct_num_count = correct_counters[num]["correct_nums"]
+                if curr_correct_num_count < frequencies_in_answer[num]:
+                    correct_counters[num]["correct_nums"] += 1
+
+                correct_counters[num]["correct_locations"] += 1
+
             elif num in answer_as_set:
-                correct_nums += 1
+
+                # if the number exists in the combination overall,
+                # check if the current correct count is above the frequency of the number in the combo overall
+                # if it is, do not increment that number's correct number count
+                curr_correct_num_count = correct_counters[num]["correct_nums"]
+
+                if curr_correct_num_count < frequencies_in_answer[num]:
+                    correct_counters[num]["correct_nums"] += 1
+
             curr_index += 1
 
-        if correct_nums == 4 and correct_locations == 4:
+        for value in correct_counters.values():
+            correct_nums += value["correct_nums"]
+            correct_locations += value["correct_locations"]
+
+        # TODO: add a comparison of the two lists here to check for a win and succeed fast at the top
+        if correct_nums == self.count and correct_locations == self.count:
             won = True
 
         return {
